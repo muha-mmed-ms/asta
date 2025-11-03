c4b7947d12dfe801b5a9f3339344f8360597a8cd
Muhammed Ijas <muhammed.majeed@vvtsolutions.in>
2025-11-03 14:54:21 +0530

    HERO component compp=leted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
+import WhoWeAre from './components/WhoWeAre'
 import Features from './components/Features'
 import About from './components/About'
 import Testimonials from './components/Testimonials'
@@ -8,10 +9,11 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-full">
-      <Header />
+    <div className="min-h-full bg-primary text-white">
+      {/* <Header /> */}
       <main>
         <Hero />
+        <WhoWeAre />
         <Features />
         <About />
         <Testimonials />
